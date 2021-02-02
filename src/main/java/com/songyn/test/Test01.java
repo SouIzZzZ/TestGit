612d98890e6c5c56dc152154a8e90d8dcec367dd
song <1470348561@qq.com>
2021-02-03 00:54:12 +0800

    003

--- src/main/java/com/songyn/test/Test01.java
+++ src/main/java/com/songyn/test/Test01.java
@@ -2,10 +2,6 @@ package com.songyn.test;
 
 public class Test01 {
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println(123);
-=======
-		System.out.println(321);
->>>>>>> branch 'master' of https://github.com/SouIzZzZ/TestGit.git
 	}
 }
